e61ec56473e05b7e914cce58f491a041b6a7485e
kx1bx1 <flamboyance@atomicmail.io>
2026-01-09 19:05:37 +0000

    Add Prettier and ESLint configs

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        lucide: "readonly",
+        tailwind: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          caughtErrorsIgnorePattern: "^_",
+        },
+      ],
+      "no-console": "off",
+    },
+  },
+];
